70fd514cca6348d2eed3ee68d60a3491a2b31e90
s1f102103707 <s1f102103707@iniad.org>
2023-11-20 18:00:58 +0900

    日本語

--- backend/server.ts
+++ backend/server.ts
@@ -1,27 +1,31 @@
 import express from "express";
 import axios from "axios";
 import cheerio from "cheerio";
+import iconv from "iconv-lite";
 
 const app: express.Express = express();
 const port = 8000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello, world!");
-});
+// app.get("/", (req: express.Request, res: express.Response) => {
+//   res.send("Hello, world!");
+// });
 
 app.get("/scrape", async (req, res) => {
   try {
     const url = "https://db.netkeiba.com/race/202305040911/";
-    const response = await axios.get(url);
-    const html = response.data;
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    const html = iconv.decode(response.data, "Shift_JIS"); // サイトの文字コードに応じて変更
 
     const $ = cheerio.load(html);
     const tableHTML = $("table").html();
 
-    res.send(tableHTML);
+    // HTMLのmetaタグにcharsetを追加する
+    const finalHTML = `<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body>${tableHTML}</body></html>`;
+
+    res.header("Content-Type", "text/html; charset=utf-8");
+    res.send(finalHTML);
   } catch (error: any) {
-    // エラーオブジェクトの型をanyに指定することでエラーを解決できる場合もあります
-    res.status(500).send((error as Error).message); // エラーオブジェクトをError型にキャスト
+    res.status(500).send((error as Error).message);
   }
 });
 
